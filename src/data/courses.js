f87e90cc6d78eab2a77a72e3a56c1e49b60d8856
calvinucheoma <calvinchukwuma@gmail.com>
2023-03-26 13:21:48 +0100

    finished design of courses page

---
+++ src/data/courses.js
@@ -0,0 +1,87 @@
+import leonard from '../assets/png/Leonard.png';
+import designUX from '../assets/png/tutoring.png';
+import audience from '../assets/png/audience.png';
+import jeffrey from '../assets/png/Jeffrey.png';
+import claretta from '../assets/png/Clareeta.png';
+import jessica from '../assets/png/Jessica.png';
+import samuel from '../assets/png/Samuel.png';
+import adam from '../assets/png/Adam.png';
+import html from '../assets/png/html.png';
+import github from '../assets/png/github.png';
+import marketing from '../assets/png/marketing.png';
+import analysis from '../assets/png/analysis.png';
+
+export const courses = [
+  {
+    img: designUX,
+    courseName: 'design',
+    rating: '4.7k',
+    numOfStudents: '(32.7k+)',
+    title: 'introduction to user research in ux design',
+    courseLength: '23hrs 50mins',
+    numOfLessons: '15 lessons',
+    tutorImage: leonard,
+    tutorName: 'leonard victor',
+    price: '$15.00',
+  },
+  {
+    img: audience,
+    courseName: 'marketing',
+    rating: '4.7k',
+    numOfStudents: '(8.7k+)',
+    title: 'introduction to  new marketing audience ',
+    courseLength: '22hrs 30mins',
+    numOfLessons: '22 lessons',
+    tutorImage: jeffrey,
+    tutorName: 'Jeffrey williams',
+    price: '$32.00',
+  },
+  {
+    img: html,
+    courseName: 'development',
+    rating: '4.7k',
+    numOfStudents: '(12.7k+)',
+    title: 'introduction to html, css & bootstrap',
+    courseLength: '45hrs 50mins',
+    numOfLessons: '55 lessons',
+    tutorImage: claretta,
+    tutorName: 'claretta mason',
+    price: '$55.00',
+  },
+  {
+    img: github,
+    courseName: 'development',
+    rating: '4.7k',
+    numOfStudents: '(32.7k+)',
+    title: 'introduction to javascript, git & GitHub',
+    courseLength: '30hrs 50mins',
+    numOfLessons: '22 lessons',
+    tutorImage: jessica,
+    tutorName: 'Jessica duke',
+    price: '$45.00',
+  },
+  {
+    img: marketing,
+    courseName: 'marketing',
+    rating: '4.7k',
+    numOfStudents: '(4.7k+)',
+    title: 'introduction to outroom  marketing analysis',
+    courseLength: '33hrs 50mins',
+    numOfLessons: '26 lessons',
+    tutorImage: samuel,
+    tutorName: 'Samuel jacobs',
+    price: '$25.00',
+  },
+  {
+    img: analysis,
+    courseName: 'marketing',
+    rating: '4.7k',
+    numOfStudents: '(15.7k+)',
+    title: 'introduction to live  marketing analysis',
+    courseLength: '10hrs 50mins',
+    numOfLessons: '32 lessons',
+    tutorImage: adam,
+    tutorName: 'adam smith',
+    price: '$25.00',
+  },
+];
--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,9 +1,88 @@
 import './Courses.scss';
+import birdShape from '../../assets/svg/birdShape.svg';
+import { courses } from '../../data/courses';
+import { Avatar } from '@mui/material';
+import { FiClock } from 'react-icons/fi';
+import { VscBook } from 'react-icons/vsc';
+import { AiFillStar } from 'react-icons/ai';
 
 const Courses = () => {
   return (
     <div className="courses" id="courses">
-      Courses
+      <div className="courses__header">
+        <h1>browse our popular courses</h1>
+        <p>
+          high-defination video is video of higher resolution and quality than
+          standard definition.
+          <br /> while there’s no standard meaning for high definition,
+          generally any standard video image
+        </p>
+        <img src={birdShape} alt="bird shape svg" className="bird-shape" />
+      </div>
+
+      <div className="courses__body">
+        <div className="courses__body__links">
+          <h5 className="active-link">all categories </h5>
+          <h5>design</h5>
+          <h5>development</h5>
+          <h5>marketing</h5>
+        </div>
+
+        <div className="courses__body__section">
+          {courses.map((course) => {
+            const {
+              img,
+              courseName,
+              rating,
+              numOfStudents,
+              title,
+              courseLength,
+              numOfLessons,
+              tutorImage,
+              tutorName,
+              price,
+            } = course;
+            return (
+              <div className="courses__body__section__course">
+                <img src={img} alt="course banner img" />
+                <div className="course__title__info">
+                  <h6>{courseName}</h6>
+                  <span>
+                    <i>
+                      <AiFillStar />
+                    </i>
+                    <p>{rating}</p> <p>{numOfStudents}</p>
+                  </span>
+                </div>
+                <h2>{title}</h2>
+                <div className="course__runtime__info">
+                  <span>
+                    <i>
+                      <FiClock />
+                    </i>
+                    <p>{courseLength}</p>
+                  </span>
+                  <span>
+                    <i>{VscBook}</i>
+                    <p>{numOfLessons}</p>
+                  </span>
+                </div>
+                <div className="course__tutor__info">
+                  <span>
+                    <Avatar
+                      src={tutorImage}
+                      alt="image of tutor"
+                      className="tutor-avatar"
+                    />
+                    <p className="tutor-name">{tutorName}</p>
+                  </span>
+                  <p>{price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
