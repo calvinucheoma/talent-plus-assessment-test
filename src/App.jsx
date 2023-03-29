e64a88df09c4a1ae1cdd2b92e59342d88788a56a
calvinucheoma <calvinchukwuma@gmail.com>
2023-03-29 13:28:38 +0100

    finished media queries for mobile and tablet devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import './App.scss';
+// import { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, AboutUs, Courses, Testimonial, Community } from './pages';
 import FAQs from './pages/FAQs/FAQs';
@@ -9,6 +10,28 @@ function App() {
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth' });
   }
+
+  // const [windowSize, setWindowSize] = useState({
+  //   width: window.innerWidth,
+  //   height: window.innerHeight,
+  // });
+
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowSize({
+  //       width: window.innerWidth,
+  //       height: window.innerHeight,
+  //     });
+  //   }
+
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
+
+  /* Add these lines of code inside any component of your react app
+        <p>Window width: {windowSize.width}</p>
+        <p>Window height: {windowSize.height}</p>
+         //Use windowSize.width and windowSize.height for media queries */
 
   return (
     <div className="app">
